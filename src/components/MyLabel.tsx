b7db4dba1be88e7bdcd7462c0e4d2c4fd523fe1c
Daniel Unapanta <dunapanta@outlook.es>
2022-10-23 19:36:23 -0500

    Initial commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,41 @@
+import "./mylabel.css";
+
+export interface MyLabelProps {
+  /**
+   * Mensaje a mostrar en el label
+   */
+  label: string;
+  /**
+   * Este es el tamaño del label
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Si es true, el texto se muestra en mayúsculas
+   */
+  allCaps?: boolean;
+  /**
+   * Color del texto
+   */
+  color?: "primary" | "secondary" | "tertiary";
+  /**
+   * Color customizado
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  label = "No Label",
+  size = "h1",
+  allCaps = false,
+  color = "primary",
+  fontColor = "black",
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
