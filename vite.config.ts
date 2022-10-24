7cc3f5b801246b267e136fae5de5815d5f3e31e4
Daniel Unapanta <dunapanta@outlook.es>
2022-10-23 20:00:46 -0500

    Add vite config for sb deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()]
 })
+
+module.exports = {
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = './';
+    }
+
+    return config;
+  },
+};
