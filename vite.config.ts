ba0d42eeef178a8debf14802a5fd836cb3198f4a
Daniel Unapanta <dunapanta@outlook.es>
2022-10-23 20:09:46 -0500

    Add viteFinal on main

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()]
 })
-
-module.exports = {
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = './';
-    }
-
-    return config;
-  },
-};
